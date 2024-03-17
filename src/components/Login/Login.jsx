0897a0136f26a66ae4776dfd34fd1916c18cebb4
jmarribas <jomuarribas@gmail.com>
2024-03-17 09:39:25 +0100

    vercel.json

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import { useApi } from '../../hooks/useApi'
 import { Link } from 'react-router-dom'
 import './Login.css'
+import Loader from '../Loader/Loader'
 
 const Login = () => {
   const {
@@ -9,7 +10,7 @@ const Login = () => {
     handleSubmit,
     formState: { errors: formErrors },
   } = useForm()
-  const { apiFetch } = useApi()
+  const { apiFetch, isLoading } = useApi()
 
   const submit = async (data) => {
     const route = 'users/login'
@@ -27,51 +28,56 @@ const Login = () => {
   }
 
   return (
-    <section id='login'>
-      <img src='/assets/logo.webp' alt='Logotipo' />
-      <h2>Login:</h2>
-      <form onSubmit={handleSubmit(submit)}>
-        <div>
-          <label htmlFor='username'>Nombre de usuario:</label>
-          <input
-            type='text'
-            id='username'
-            autoComplete='current-username'
-            {...register('username', {
-              required: 'Debes poner un nombre de usuario',
-            })}
-            className={formErrors.username ? 'inputError' : ''}
-          />
-          {formErrors.username && (
-            <p className='errors'>{formErrors.username.message}</p>
-          )}
-        </div>
-        <div>
-          <label htmlFor='password'>Contraseña:</label>
-          <input
-            type='password'
-            id='password'
-            autoComplete='current-password'
-            {...register('password', {
-              required: 'Debes poner una contraseña',
-              pattern: {
-                value: /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/,
-                message: '8 Caracteres, 1 mayúscula y 1 minúscula',
-              },
-            })}
-            className={formErrors.password ? 'inputError' : ''}
-          />
-          {formErrors.password && (
-            <p className='errors'>{formErrors.password.message}</p>
-          )}
-        </div>
+    <>
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <section id='login'>
+          <img src='/assets/logo.webp' alt='Logotipo' />
+          <h2>Login:</h2>
+          <form onSubmit={handleSubmit(submit)}>
+            <div>
+              <label htmlFor='username'>Nombre de usuario:</label>
+              <input
+                type='text'
+                id='username'
+                autoComplete='current-username'
+                {...register('username', {
+                  required: 'Debes poner un nombre de usuario',
+                })}
+                className={formErrors.username ? 'inputError' : ''}
+              />
+              {formErrors.username && (
+                <p className='errors'>{formErrors.username.message}</p>
+              )}
+            </div>
+            <div>
+              <label htmlFor='password'>Contraseña:</label>
+              <input
+                type='password'
+                id='password'
+                autoComplete='current-password'
+                {...register('password', {
+                  required: 'Debes poner una contraseña',
+                  pattern: {
+                    value: /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/,
+                    message: '8 Caracteres, 1 mayúscula y 1 minúscula',
+                  },
+                })}
+                className={formErrors.password ? 'inputError' : ''}
+              />
+              {formErrors.password && (
+                <p className='errors'>{formErrors.password.message}</p>
+              )}
+            </div>
 
-        <button className='button' type='submit'>
-          Entrar
-        </button>
-      </form>
-      <Link to='/register'>Registrate</Link>
-    </section>
+            <button className='button' type='submit'>
+              Entrar
+            </button>
+          </form>
+          <Link to='/register'>Registrate</Link>
+        </section>
+      )}
+    </>
   )
 }
 
